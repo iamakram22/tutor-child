efc53e93aaef870086db228becbfe67d5a56b81d
Akram <iamakram22@gmail.com>
2024-03-04 22:11:04 +0530

    version constant added in functions.php

--- functions.php
+++ functions.php
@@ -8,9 +8,9 @@
  */
 function child_enqueue_styles() {
 
-	wp_enqueue_style( 'tutor-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('main-css'), '1.0.0', 'all' );
+	wp_enqueue_style( 'tutor-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('main-css'), TUTOR_STARTER_VERSION, 'all' );
 	
-	wp_enqueue_script( 'custom-script', get_stylesheet_directory_uri() . '/script.js', array('jquery'), '1.0.0', true);
+	wp_enqueue_script( 'custom-script', get_stylesheet_directory_uri() . '/script.js', array('jquery'), TUTOR_STARTER_VERSION, true);
 
 }
 
