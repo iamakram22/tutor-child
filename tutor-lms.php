c3a85cc5dc75794158e854bb0907a4cf0cf32f47
Mohd Akram <iamakram22@gmail.com>
2024-06-09 16:20:33 +0530

    AVAS reuest modifications

--- tutor-lms.php
+++ tutor-lms.php
@@ -3,6 +3,7 @@ add_filter('tutor_dashboard/bottom_nav_items', 'site_setting_dashboard');
 add_filter('tutor_dashboard/instructor_nav_items', 'instructor_navigation');
 add_filter('tutor_dashboard/nav_items/settings/nav_items', 'dashboard_navigation');
 add_action('tutor_backend_profile_fields_after', 'tutor_custom_profile_fields');
+add_action('tutor_dashboard/nav_items', 'practice_portal_link');
 
 /**
  * Modify instructor dashboard navigations
@@ -46,6 +47,18 @@ function site_setting_dashboard($links)
             'icon' => 'tutor-icon-website'
         );
     }
+    return $links;
+}
+
+function practice_portal_link($links) {
+	$user = get_current_user_id();
+	if(current_user_can('tutor_instructor')) {
+		$links['practice'] = array(
+            'title' => __('Practice Portal', 'tutor'),
+            'icon' => 'tutor-icon-external-link',
+			'url' => 'https://app.iivapracticeportal.com/'
+        );
+	}
     return $links;
 }
 
